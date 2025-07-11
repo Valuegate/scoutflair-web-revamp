6fb675fbb66204752b493874332df6b11a756296
OGUNKOYA GBENGA <davidogunkoya80@gmail.com>
2025-07-11 23:58:19 +0000

    Good we are almost there but the image should ontop of that dotted desig

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -18,6 +18,7 @@ export function AboutSection() {
         <div className="grid md:grid-cols-2 gap-12 lg:gap-20 items-center mb-16 md:mb-24">
           {/* Left Column: Image */}
           <div className="relative md:order-first h-[506px]">
+            <div className="absolute top-9 left-9 w-[450px] h-[470px] border-4 border-dashed border-[#E5AA42] rounded-xl opacity-75"></div>
             <div className="absolute top-0 left-0 w-[440px] h-[460px]">
               <Image
                 src="/images/Image_1598_2263.png"
@@ -28,7 +29,6 @@ export function AboutSection() {
                 sizes="(max-width: 768px) 100vw, 440px"
               />
             </div>
-            <div className="absolute top-9 left-9 w-[450px] h-[470px] border-4 border-dashed border-[#E5AA42] rounded-xl opacity-75"></div>
           </div>
 
           {/* Right Column: Content */}
